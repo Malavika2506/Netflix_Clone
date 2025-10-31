a288184fb4649d375e09dad3df56dd5fdd218442
Malavika2506 <malavika2506@gmail.com>
2025-10-31 13:05:24 +0530

    updated file

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
-// firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA21JADp9Rcq_aM45dwuMxsjRTPaztNmSk",
-  authDomain: "netflix-clone-309c8.firebaseapp.com",
-  projectId: "netflix-clone-309c8",
-  storageBucket: "netflix-clone-309c8.appspot.com", 
-  messagingSenderId: "793017495527",
-  appId: "1:793017495527:web:0360431426e91c9a1b9742"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
